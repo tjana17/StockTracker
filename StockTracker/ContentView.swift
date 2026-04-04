7487914c0d57169ca73ff828a50173bd9b3b11f4
Janarthanan Kannan <tjana17@gmail.com>
2026-04-04 11:35:16 +0530

    Initial Commit

---
+++ StockTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StockTracker
+//
+//  Created by Janarthanan Kannan on 04/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StockTracker/StockTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockTrackerApp.swift
+//  StockTracker
+//
+//  Created by Janarthanan Kannan on 04/04/26.
+//
+
+import SwiftUI
+
+@main
+struct StockTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
